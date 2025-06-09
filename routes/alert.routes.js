af95e45ec98c18dffcdd1ea10ce1a159b7f2b160
Mert Kaan Alpan <mertkaanalpan@gmail.com>
2025-06-09 18:36:15 +0300

    alert route fix

--- routes/alert.routes.js
+++ routes/alert.routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const alertController = require('../controllers/alert.controller');
+const verifyToken = require('../middleware/verifyToken');
 
 // Get all alerts
-router.get('/', alertController.getAllAlerts);
+router.get('/', verifyToken, alertController.getAllAlerts);
+
+// Create Alert
+router.post('/', verifyToken, alertController.createAlert);
 
 // Get alert by ID
-router.get('/:id', alertController.getAlertById);
+router.get('/:id', verifyToken, alertController.getAlertById);
 
 // Resolve alert by ID
-router.patch('/:id/resolve', alertController.resolveAlert);
+router.patch('/:id/resolve', verifyToken, alertController.resolveAlert);
 
 module.exports = router;
